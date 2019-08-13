730d624cbd2e249a30ac534aedc52e8c214f4fe9
Arseniy Virachev <trigger239@users.noreply.github.com>
2019-08-13 23:24:10 +0300

    utf8 bug fix

--- CardBluffServer/util/unicode.cpp
+++ CardBluffServer/util/unicode.cpp
@@ -1,3 +1,3 @@
 #include "unicode.h"
 
-std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+std::wstring_convert<std::codecvt_utf8<wchar_t, 0x10ffff, std::little_endian>, wchar_t> converter;
